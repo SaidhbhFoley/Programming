f468d8f12e7c36f3042d6a581ab846aa72b943f9
Saidhbh Foley <G00398288@gmit.ie>
2021-02-17 21:50:18 +0000

    new work

---
+++ my-work/boolean.py
@@ -0,0 +1,9 @@
+#Messing around with Boolean Statements
+#Author: Saidhbh Foley
+
+isAlive = False
+print (isAlive)
+
+a = 32
+isEven = ((a % 2) == 0)
+print ("is", a, "even?", isEven)
---
+++ my-work/testif.py
@@ -0,0 +1,34 @@
+#Programme to show if statement
+#Saidhbh Foley
+
+if True:
+    # statements
+    print ("condition is true")
+
+b = 3
+
+if b == 2:
+    print ("b is equal to 2")
+
+else:
+    print ("b is not equal to 2")
+
+a = 2
+
+if a != 2:
+    print ("I hope this is not displayed")
+
+else:
+    print ("2 is not equal to 2 is false")
+
+aNumber = 13
+
+if (aNumber % 2) == 0:
+    print (aNumber, "is even")
+
+elif (aNumber % 3) == 0:
+    print (aNumber, "is divisable by 3")
+else: 
+    print (aNumber, "is not even or divisable by 3")
+
+print ("This will always be outputted")
